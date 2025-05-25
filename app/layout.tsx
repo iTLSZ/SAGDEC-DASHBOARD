e20303212efa32489311d0711d37ae975bf439a2
iTLZS <lalopezsuarez@uniguajira.edu.co>
2025-05-25 02:40:38 -0500

    Tu mensaje de lo que hiciste

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Dashboard Interactivo ',
+  title: 'Dashboard Interactivo',
   description: 'Luis Lopez',
   generator: 'LALS',
 }
